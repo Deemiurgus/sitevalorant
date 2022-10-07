c289432071dc4897598a9b59cdd0bef9c82329c4
Deemiurgus <iury_zak@outlook.com>
2022-10-07 10:21:54 -0300

    attu

--- cadastro.js
+++ cadastro.js
@@ -16,3 +16,13 @@ function Enviar(){
     Bank.push(usuario)
     console.log(Bank)
 }
+
+function login(){
+    
+let Name = document.getElementById('Name').value;
+let Senha1 = document.getElementById('Senha').value;
+    if((Name == "adm" || Name == "adm456" || Name == "adm@gmail.com") && Senha1 == "123"){
+        alert("Logado!")   
+        window.location.href ="/home.html"
+    }
+}
